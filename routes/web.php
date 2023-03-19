5b0995db272852bf9e3f6f95f316ace3fc1685f1
fanto6314 <antonio.foresta04@gmail.com>
2023-03-19 21:47:52 +0100

    gg2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\testSession;
+use App\Http\Controllers\registerFlyts;
+use App\Http\Controllers\handleLogout;
+use App\Http\Controllers\homeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [homeController::class, 'home']);
 
 Route::get('/login', function () {
     return view('login');
@@ -24,3 +28,18 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('register');
 });
+
+Route::get('/tests', function () {
+    return view('testsession');
+});
+
+
+Route::post('/registerUserRoute', [RegisterController::class, 'registerUser'])->name('registerUser');
+
+Route::post('/loginUserRoute', [LoginController::class, 'loginUser'])->name('loginUser');
+
+Route::post('/registerFlytsRoute', [registerFlyts::class, 'registerFlyts'])->name('registerFlyts');
+
+Route::post('/handleLogoutRoute', [handleLogout::class, 'handleLogout'])->name('handleLogout');
+
+Route::post('/testSessionRoute', [testSession::class, 'testSession'])->name('testSession');
